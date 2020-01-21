2c39aa07a0d755302d540e3f8bfa9eddb70e01d2
Paul Luna <paulluna0215@gmail.com>
2020-01-20 21:30:28 -0800

    Added signup route and created simple signup component

---
+++ src/views/Signup.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="signupForm">
+        <form>
+            <label>Email: </label>
+            <input type="email" name="email">
+            <label>Password: </label>
+            <input type="password" name="password">
+            <input type="submit" value="Signup">
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Signup'
+}
+</script>
+
+<style scoped>
+.signupForm {
+    display:block;
+}
+
+label {
+    display: inline-block;
+}
+</style>
