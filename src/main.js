cec113156c27567d91ff19b0391c75e3a81a6939
Paul Luna <paulluna0215@gmail.com>
2020-02-02 12:59:17 -0800

    installed axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+// import Axios from 'axios'
+
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+// Vue.prototype.$http = Axios
 
 new Vue({
   router,
