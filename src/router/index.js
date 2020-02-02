9891d00799cc84c787e5f2b0d59ccc0027df73dc
Paul Luna <paulluna0215@gmail.com>
2020-02-02 13:02:56 -0800

    added login and userprofile

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+import UserProfile from '../views/UserProfile.vue'
 
 Vue.use(VueRouter)
 
@@ -17,12 +19,19 @@ const routes = [
     component: SignUp
   },
   {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: UserProfile
   }
 ]
 
