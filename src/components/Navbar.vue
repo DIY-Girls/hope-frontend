b7970d969b23a249fd13c98ee0e0f4a5223a4f69
angelaa02 <asanchez0641@mymail.lausd.net>
2020-03-11 11:18:25 -0700

    changed birthday

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,7 @@
         <router-link to="/about">About</router-link>
         <router-link to="/profile">Profile</router-link>
         <router-link to="/login">Login</router-link>
+        <router-link to="/signup">SignUp</router-link>
         <button v-on:click="logout">Logout</button>
     </div>
 </template>
