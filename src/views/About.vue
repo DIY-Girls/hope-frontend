0a00b728a0495caf68efea6f8add5fdd34322f49
ceceiling <ceci03r@gmail.com>
2020-03-11 10:53:44 -0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,86 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template> <div>
+    <div class="about">
+        <h1>This is an about page</h1>
+    </div>
+    <!-- The App Section -->
+    <!-- The App Section -->
+    <center><iframe src="https://drive.google.com/file/d/1P91y6xYNlbjtEJjk_B-5-lKC9CbU1dZH/preview" width="640" height="480"></iframe></center>
+<div class="container">
+<div class="row">
+    <h1 class="xlarge-font" style="color: #00000; text-shadow: 2px 2px #f3e6fc"><b>Mission Statement</b></h1>
+<div class="column-66">
+    <h1 class="large-font" style="color:#382345; text-shadow:2px 2px #A9A9A9"><b>The Goal</b></h1>
+    <p>The SFHS Invent-Team is developing an accessory that will make females between the ages of 13-19 feel a lot safer in their own community. This accessory will include a variety of components that allow them to call for help under ominous situations when they might need it most. The invention will be instrumental in getting her the help she needs if she ever finds herself in a dangerous situation. By pressing the alert button
+    on her accessory, she will trigger an alert message that will be shared
+    with her emergency contacts and/or law enforcement that includes her GPS location. For many young people, their privacy is very important which is why our device will only deliver the GPS location when the alert button is pressed. If the button isn’t pressed, the GPS location will not be shared, ensuring their privacy remains intact. In addition, when the button is pressed, a hidden camera will record what the young female is experiencing.
+    The recording will be saved and sent to law enforcement for further investigation. The device will be powered by renewable energy so that they will be able to charge the device anywhere they might be, whether they have an outlet around or not. All of these components will be disguised as an accessory, therefore the perpetrator will have no idea
+    that the young female is calling for help.</p>
+    <!-- <button class="button">Download Application</button> -->
+</div>
+</div>
+</div>
+    <!-- Clarity Section -->
+<div class="container" style="background-color:#f3e6fc">
+<div class="row">
+<div class="column-33">
+    <iframe src="https://drive.google.com/file/d/115Kg9Ok0pOGsoX79Amjf3ZXS-t_QbV5u/preview" width="600" height="500"></iframe>
+</div>
+<div class="column-66">
+<div class="right">
+    <h1 class="large-font" style="color:#FFFFFF; text-shadow:2px 2px #000000"><b>The Team</b></h1>
+    <p>The SFHS Invent Team is an all-female group made up of 24 students in grades 9-12. The females are students at San Fernando High School (SFHS)
+    and have demonstrated excellent leadership, critical thinking and problem-solving skills, among other qualities which enhance the team’s performance. Most team members have background experience in coding or are enrolled in a Computer Science class, a skill the team will put into
+    practice while creating the project. Most of the girls have participated in Engineering Dynamics class at SFHS, in which we learned about 3D design, electrical engineering, programing using Arduinos, soldering, sewing, and
+    coding.</p>
+    <!--  <button class="button" style="background-color:red">Read More</button>-->
+</div>
+</div>
+</div>
+</div>
+
+    <!-- The App Section -->
+<div class="container">
+<div class="row">
+<div class="column-66">
+    <h1 class="large-font" style="color:#e6ccff; text-shadow:2px 2px #000000"><b>The Project</b></h1>
+    <p>The SFHS Invent-Team is developing an accessory that will make females between the ages of 13-19 feel a lot safer in their own community. This accessory will include a variety of components that allow them to call for help under ominous situations when they might need it most. The invention will be instrumental in getting her the help she needs if she ever finds herself in a dangerous situation. By pressing the alert button
+    on her accessory, she will trigger an alert message that will be shared
+    with her emergency contacts and/or law enforcement that includes her GPS location. For many young people, their privacy is very important which is why our device will only deliver the GPS location when the alert button is pressed. If the button isn’t pressed, the GPS location will not be shared, ensuring their privacy remains intact. In addition, when the button is pressed, a hidden camera will record what the young female is experiencing.
+    The recording will be saved and sent to law enforcement for further investigation. The device will be powered by renewable energy so that they will be able to charge the device anywhere they might be, whether they have an outlet around or not. All of these components will be disguised as an accessory, therefore the perpetrator will have no idea
+    that the young female is calling for help.</p>
+    <!-- <button class="button">Download Application</button> -->
+</div>
+<div class="column-33">
+    <iframe src="https://drive.google.com/file/d/1dFS9zVSAVQYHziKKLzO4qX3sDywyhT1I/preview" width="450" height="480"></iframe>
+</div>
+</div>
+</div>
+<div>
+<div class="container" style="background-color: #f3e6fc">
+</div>
+<div class="row">
+</div>
+<div class="column-33">
+</div>
+    <div class="column-66">
+    <div class="right2">
+        <h1 class="large-font" style="color:#FFFFFF; text-shadow:2px 2px #000000"><b>The Neighborhood</b></h1>
+        <p>
+            As an all-female Invent-Team living in the San Fernando Valley, many of us have experienced discomfort by the mere thought of walking home after practice in the late afternoon. Women and young girls should feel safe in
+            their community and our device will give them an added level of security
+            they’ve never had before. With the alarming occurrences we hear on the
+            news about young women being abducted for sex-trafficking, rape, or
+            torture, we are concerned that at any moment, that young girl on the
+            news could be one of us. Feeling safer when we walk home from school, or
+            even simply spending time in our own community, will allow us to continue
+            with our day-to-day life giving us the opportunity to grow.
+        </p>
+    <!--  <button class="button" style="background-color:red">Read More</button> -->
+    </div>
+    </div>
+</div>
+</div>
+
 </template>
 
 <script>
